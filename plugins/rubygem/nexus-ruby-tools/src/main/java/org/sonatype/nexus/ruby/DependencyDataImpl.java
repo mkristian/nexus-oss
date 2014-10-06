c0325fff868ffec2467288681bf299b44229ac60
Jason Dillon <jason@planet57.com>
2014-10-06 16:12:36 -0700

    drop pointless comments

--- plugins/rubygem/nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/DependencyDataImpl.java
+++ plugins/rubygem/nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/DependencyDataImpl.java
@@ -25,40 +25,26 @@ public class DependencyDataImpl
 {
   private final long modified;
 
-  public DependencyDataImpl(ScriptingContainer scriptingContainer,
-                            Object dependencies, long modified)
-  {
+  public DependencyDataImpl(ScriptingContainer scriptingContainer, Object dependencies, long modified) {
     super(scriptingContainer, dependencies);
     this.modified = modified;
   }
 
-  /* (non-Javadoc)
-   * @see org.sonatype.nexus.ruby.Dependencies#versions(boolean)
-   */
   @Override
   public String[] versions(boolean prereleased) {
     return callMethod("versions", prereleased, String[].class);
   }
 
-  /* (non-Javadoc)
-   * @see org.sonatype.nexus.ruby.Dependencies#platform(java.lang.String)
-   */
   @Override
   public String platform(String version) {
     return callMethod("platform", version, String.class);
   }
 
-  /* (non-Javadoc)
-   * @see org.sonatype.nexus.ruby.Dependencies#name()
-   */
   @Override
   public String name() {
     return callMethod("name", String.class);
   }
 
-  /* (non-Javadoc)
-   * @see org.sonatype.nexus.ruby.Dependencies#modified()
-   */
   @Override
   public long modified() {
     return modified;
